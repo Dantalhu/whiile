d354dbbe8146b329ed5806dc99b45bf23a0c624a
dantalhu <maximilyi@yandex.ru>
2025-10-05 14:57:16 +0500

    create Arena

--- index.ts
+++ index.ts
@@ -11,48 +11,65 @@ function getRandomName(): string {
 interface Weapon {
     damage: number;
     attack(): number;
+    getName(): string;
 }
 
 class Sword implements Weapon {
     damage: number = 20;
+    
     attack(): number {
         const chance = Math.random();
         if (chance <= 0.25) {
+            console.log('  (Критический удар!)');
             return this.damage * 2;
         }
         return this.damage;
+    }
+    
+    getName(): string {
+        return 'Меч';
     }
 }
 
 class Spear implements Weapon {
     damage: number = 15;
+    
     attack(): number {
         return this.damage;
+    }
+    
+    getName(): string {
+        return 'Копье';
     }
 }
 
 class Axe implements Weapon {
     rageDamage: number = 0;
     rageBonus: number = 2;
-    damage: number = 25;
+    damage: number = 12;
+    
     attack(): number {
         const totalDamage = this.damage + this.rageDamage;
         this.rageDamage += this.rageBonus;
         return totalDamage;
+    }
+    
+    getName(): string {
+        return 'Топор';
     }
 }
 
 class Gladiator {
     constructor(public name: string, public health: number, public weapon: Weapon) {}
 
-    attack(target: Gladiator): void {
+    attack(target: Gladiator): number {
         const damage = this.weapon.attack();
         target.takeDamage(damage);
+        return damage;
     }
 
     takeDamage(damage: number): void {
         this.health -= damage;
-        console.log(`${this.name} получил ${damage} урона. Осталось ${this.health}`);
     }
 
     isDead(): boolean {
@@ -60,8 +77,11 @@ class Gladiator {
     }
 
     showStats(): void {
-        const weaponName = this.weapon.constructor.name;
-        console.log(`Имя: ${this.name}, Здоровье: ${this.health}, Оружие: ${weaponName}`);
+        console.log(`- ${this.name} (Здоровье: ${this.health}, Оружие: ${this.weapon.getName()})`);
+    }
+    
+    getInfo(): string {
+        return `${this.name} (${this.weapon.getName()})`;
     }
 }
 
@@ -76,6 +96,82 @@ function getRandomWeapon(): Weapon {
     }
 }
 
+class Arena {
+    gladiators: Gladiator[];
+
+    constructor(gladiators: Gladiator[]) {
+        this.gladiators = gladiators;
+    }
+
+    start(): void {
+        console.log('Турнир начинается!\n');
+        console.log('Участники:');
+        this.gladiators.forEach(gladiator => gladiator.showStats());
+        console.log('');
+        
+        this.runTournament();
+    }
+
+    runTournament(): void {
+        let participants = [...this.gladiators];
+        let battleNumber = 1;
+
+        while (participants.length > 1) {
+            const newParticipants: Gladiator[] = [];
+
+            for (let i = 0; i < participants.length; i += 2) {
+                if (i + 1 < participants.length) {
+                    const fighter1 = participants[i];
+                    const fighter2 = participants[i + 1];
+                    
+                    console.log(`--- Бой ${battleNumber} ---`);
+                    const winner = this.battle(fighter1, fighter2);
+                    newParticipants.push(winner);
+                    console.log('');
+                    battleNumber++;
+                } else {
+                    newParticipants.push(participants[i]);
+                }
+            }
+
+            participants = newParticipants;
+        }
+
+        this.announceChampion(participants[0]);
+    }
+
+    battle(fighter1: Gladiator, fighter2: Gladiator): Gladiator {
+        console.log(`${fighter1.getInfo()} против ${fighter2.getInfo()}\n`);
+
+        while (!fighter1.isDead() && !fighter2.isDead()) {
+            // Ход первого бойца
+            const damage1 = fighter1.attack(fighter2);
+            console.log(`  ${fighter1.name} наносит ${damage1} урона ${fighter2.name}!`);
+            
+            if (fighter2.isDead()) {
+                console.log(`\n${fighter2.name} побежден!`);
+                return fighter1;
+            }
+
+            // Ход второго бойца
+            const damage2 = fighter2.attack(fighter1);
+            console.log(`  ${fighter2.name} наносит ${damage2} урона ${fighter1.name}!`);
+            
+            if (fighter1.isDead()) {
+                console.log(`\n${fighter1.name} побежден!`);
+                return fighter2;
+            }
+        }
+
+        return fighter1.isDead() ? fighter2 : fighter1;
+    }
+
+    announceChampion(champion: Gladiator): void {
+        console.log('Император приветствует победителя!');
+        console.log(`Чемпион арены: ${champion.name} (Здоровье: ${champion.health}, Оружие: ${champion.weapon.getName()})`);
+    }
+}
+
 let group: Gladiator[] = [];
 
 function populateGroup(count: number) {
@@ -84,10 +180,7 @@ function populateGroup(count: number) {
         group.push(gladiator);
     }
 }
-
+// Создание гладиаторов и запуск турнира
 populateGroup(6);
-group.forEach(gladiator => gladiator.showStats());
-
-
-
-
+const arena = new Arena(group);
+arena.start();
